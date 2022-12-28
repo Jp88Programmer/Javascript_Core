eea75842f737b9a07515df21f989cfd33f2c76b7
Jayendra <parmarjayendra8860@gmail.com>
2022-12-28 19:15:34 +0530

    AddToClass_28/12

---
+++ javasciptClass.js
@@ -0,0 +1,5 @@
+//class in javascript 
+
+//class : class is the one time of the blue print of any entites like if you said that car than car has four whells,engine 
+// and sets and so on.. that is the fixed generic property to define all the car 
+
